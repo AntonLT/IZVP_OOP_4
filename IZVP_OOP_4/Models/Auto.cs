b9ed44391211f838fef634932fb372e919fd9aee
AntonLT <AntonLTeacup@gmail.com>
2021-01-31 00:53:06 +0200

    Initial commit

---
+++ IZVP_OOP_4/Models/Auto.cs
@@ -0,0 +1,24 @@
+﻿namespace IZVP_OOP_4.Models
+{
+    public class Auto
+    {
+        private string ownerFirstName;
+        private string ownerLastName;
+        private string registrationCode;
+        private string model;
+        private string color;
+
+        public Auto()
+        {
+        }
+
+        public Auto(string ownerFirstName, string ownerLastName, string registrationCode, string model, string color)
+        {
+            this.ownerFirstName = ownerFirstName;
+            this.ownerLastName = ownerLastName;
+            this.registrationCode = registrationCode;
+            this.model = model;
+            this.color = color;
+        }
+    }
+}
